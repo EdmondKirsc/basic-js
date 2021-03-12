08391f0951ac79af8d5d45adb2fbf38ab66f4044
EdmondKirsc <arzhanovpavel@mail.ru>
2021-03-12 18:37:49 +0300

    init: finish carbon-dating, count-cats, dream-team, what-seasons

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+module.exports = function dateSample(sampleActivity) {
+  const MODERN_ACTIVITY= 15; 
+  const HALF_LIFE_PERIOD= 5730;
+  const l = Math.LN2;
+  let check = Number(sampleActivity);
+  let checkOn = isNaN(check);
+
+  if (checkOn === true) {
+    return false;
+  }
+  if (typeof sampleActivity === 'number' || typeof sampleActivity === 'boolean' || typeof sampleActivity === 'undefined' || typeof sampleActivity === 'object') {
+    return false;
+  }
+  if (sampleActivity.trim().length < 1) {
+    return false;
+  }
+
+
+  if (parseFloat(sampleActivity) <= 0 || parseFloat(sampleActivity) > MODERN_ACTIVITY) {
+    return false;
+  } 
+  if (sampleActivity == undefined) {
+    return false;
+  }
+  let result = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) * HALF_LIFE_PERIOD / l;
+  
+  return Math.ceil(result);
+  
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+    
+
+  let arr = [];                                  //создаем пустой массив
+  for (let i = 0; i < members.length; i++) {
+   if (typeof members[i] === 'string') {
+     arr.push(members[i]);
+   }
+ }                                              //в пустой массив arr пушим все строковые значения из members
+
+ for (j = 0; j < arr.length; j++) {
+   arr[j] = arr[j].trim();
+ }                                              //убираем пробелы из строк в массиве arr
+
+  let firstLetters = [];
+  for (let k = 0; k < arr.length; k++) {
+    firstLetters.push(arr[k][0]);
+  }                                             //в новый массив firstLetters добавляем первые буквы строк из массива arr
+  let UpperLetters = [];
+  for (let l = 0; l < firstLetters.length; l++) {
+    UpperLetters.push(firstLetters[l].toUpperCase());
+  }                                       //в новый массив UpperLetters Добавляем заглавные буквы из массива firstLetters
+
+  let sortedArray = UpperLetters.sort();
+  let str = sortedArray.join("");
+  return str;
+
+
 };
+
