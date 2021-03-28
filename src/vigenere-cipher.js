439110ba24092703cb4574d1a69cf4e7db0411a0
EdmondKirsc <arzhanovpavel@mail.ru>
2021-03-28 22:23:52 +0300

    feat: start cypher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,71 @@
 const CustomError = require("../extensions/custom-error");
+  let ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const getLetterIndex = (letter) => ALPHABET.indexOf(letter);
+	const ALPHABET_SYMBOL_COUNT = 25;
+	const getEncryptedSymbol = (textLetter, keyLetter) => {
+		let encryptedSymbolIndex = getLetterIndex(textLetter) + getLetterIndex(keyLetter);
+		if (encryptedSymbolIndex > 25) {
+			encryptedSymbolIndex -= 26;
+		}
+		return ALPHABET[encryptedSymbolIndex];
+	}
+	const getDecryptedSymbol = (textLetter, keyLetter) => {
+  		let decryptedSymbolIndex = getLetterIndex(textLetter) + 26 - getLetterIndex(keyLetter);
+    	if (decryptedSymbolIndex > 25) {
+    		decryptedSymbolIndex -= 26;
+  		}
+  		return ALPHABET[decryptedSymbolIndex];
+	}
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor (isDirect = true) {
+      this.isDirect = isDirect;
+    }
+  	encrypt(text, key) {
+  		if (!(text && key)) {
+  			throw new Error("THROWN");
+  		}
+  		let encryptedText = '';
+  		let currentKeyIndex = 0;
+  		text = text.toUpperCase();
+  		key = key.toUpperCase();
+  		for (let i = 0; i < text.length; i++) {
+    		if(ALPHABET.includes(text[i])) {
+     			encryptedText += getEncryptedSymbol(text[i], key[currentKeyIndex]);
+      			currentKeyIndex++;
+      			if(currentKeyIndex >= key.length) {
+        			currentKeyIndex = 0;
+      			} 
+    		} else {
+      			encryptedText += text[i]
+    		}
+  		}
+      if (this.pepemennay === false) {
+        encryptedText = encryptedText.split("").reverse().join("");
+      }
+		return encryptedText;
+	}
+	decrypt(text, key) {
+  		if (!(text && key)) {
+    		throw new Error("THROWN");
+  		}
+  		let decryptedText = '';
+  		let currentKeyIndex = 0;
+  		text = text.toUpperCase();
+  		key = key.toUpperCase();
+  		for (let i = 0; i < text.length; i++) {
+    		if(ALPHABET.includes(text[i])) {
+      			decryptedText += getDecryptedSymbol(text[i], key[currentKeyIndex]);
+      			currentKeyIndex++;
+      			if(currentKeyIndex >= key.length) {
+        			currentKeyIndex = 0;
+      			} 
+    		} else {
+      			decryptedText += text[i]
+    		}
+  		}
+  		return decryptedText;
+	}
 }
 
 module.exports = VigenereCipheringMachine;
